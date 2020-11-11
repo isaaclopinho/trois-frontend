8c25578c18b59314437aec81728203a1fab2bd09
Isaac Lopes <isaaclopinho@gmail.com>
2020-11-10 21:39:05 -0300

    ticket improvements

--- src/app/shared/ticket-card/ticket-card.component.ts
+++ src/app/shared/ticket-card/ticket-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AirlineTicket } from '../airlineticket.model';
 
 @Component({
   selector: 'app-ticket-card',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-card.component.css']
 })
 export class TicketCardComponent implements OnInit {
+  
+  airlineTicket : AirlineTicket = new AirlineTicket("BSB", "CGH", new Date(), new Date(), 100, 200, "LATAM");
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getDateFormatted(date : Date){
+    let d = date.getDate();
+    let m = date.getMonth();
+    let y = date.getFullYear();
+    return d + "/" + m + "/" + y; 
+  }
+
 
 }

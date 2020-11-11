1b21a174eb9766d91f02b775c9674afba1b40217
Isaac Lopes <isaaclopinho@gmail.com>
2020-11-11 01:46:34 -0300

    guard route added in routing

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -4,13 +4,14 @@ import { ResultsComponent } from './results/results.component';
 import { PageTestComponent } from './page-test/page-test.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { GuardTestComponent } from './guard-test/guard-test.component';
 
 const routes: Routes = [
     { path: '', redirectTo:'/passagens', pathMatch: 'full'},
     
     { path: 'passagens', component: ResultsComponent },
     { path: 'teste', component: PageTestComponent },
-    { path: 'teste2', canActivate: [AuthGuard], component: PageTestComponent },
+    { path: 'teste2', canActivate: [AuthGuard], component: GuardTestComponent },
     { path: 'auth', component: LoginComponent },
 
     //{ path: 'path/:routeParam', component: MyComponent },

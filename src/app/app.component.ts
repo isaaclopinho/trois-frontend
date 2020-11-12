649b33cc652d5771a7484e1a96cde56fbb8e4691
Isaac Lopes <isaaclopinho@gmail.com>
2020-11-11 22:03:36 -0300

    nothing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -7,7 +7,7 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit{
   title = "App"
 
   constructor(private authService : AuthService){
